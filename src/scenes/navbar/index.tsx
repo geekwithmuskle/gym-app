be9c5936ff5c09201c02ebbaa31a867cdc4fd96c
geekwithmuskle <oniayobami100@gmail.com>
2024-10-16 09:08:34 +0100

    I ran the build code

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-import { nav } from "framer-motion/client";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
