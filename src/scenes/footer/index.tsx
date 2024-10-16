f39f52674713b40dcb0b94e6530d2bdff9fff139
geekwithmuskle <oniayobami100@gmail.com>
2024-10-16 08:17:54 +0100

    Finished up my home page

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/assets/Logo.png"
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer>
+        <div className="justify-content">
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
